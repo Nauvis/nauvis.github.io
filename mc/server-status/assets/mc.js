fc42575d0372dcc63e262b6df5dc62a3697bcd50
Elda <81310437+Nauvis@users.noreply.github.com>
2024-01-03 22:31:55 -0500

    playerlist test

--- mc/server-status/assets/mc.js
+++ mc/server-status/assets/mc.js
@@ -1,11 +1,18 @@
-function getOnlineStatus(serverIP, statusQuerySel, countQuerySel, divQuerySel) {
+function getOnlineStatus(serverIP, statusQuerySel, countQuerySel, listQuerySel) {
 	MinecraftAPI.getServerStatus(serverIP, {}, function (err, status) {  
-	if (err) {  
-	return document.querySelector(divQuerySel).innerHTML = 'Error loading status';  
-	}
-	document.querySelector(statusQuerySel).innerHTML = status.online ? 'Online' : 'Offline';
-	document.querySelector(countQuerySel).innerHTML = status.players.now + "/" + status.players.max;
+		if (err) {  
+			return document.querySelector(statusQuerySel).innerHTML = 'Error';  
+		}
+		document.querySelector(statusQuerySel).innerHTML = status.online ? 'Online' : 'Offline';
+		document.querySelector(countQuerySel).innerHTML = status.players.now + "/" + status.players.max;
+		if (status.players.now > 0) {
+			var playerList = "<br>";
+			for (let i = 0; i < status.players.now; i++) {
+				playerList = playerList + "- " + status.players.sample[i]["name"] + "<br>";
+			} 
+			document.querySelector(listQuerySel).innerHTML = playerList;
+		}
 	});
 }
-getOnlineStatus('mc.nauvis.dev', '.serverStatus1', '.playerCount1', '.errDiv1');
-getOnlineStatus('kate.nauvis.dev', '.serverStatus2', '.playerCount2', '.errDiv2');
+getOnlineStatus('mc.nauvis.dev', '.serverStatus1', '.playerCount1', '.playerList1');
+getOnlineStatus('kate.nauvis.dev', '.serverStatus2', '.playerCount2', '.playerList2');
